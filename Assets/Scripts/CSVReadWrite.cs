c1f4a21c45f2532d166d1d1e68d2241eb1711cc4
roy0rr <mikael.sauriol.1@ens.etsmtl.ca>
2020-12-04 01:26:58 -0500

    Last commit for 860

---
+++ Assets/Scripts/CSVReadWrite.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System;
+using UnityEngine;
+
+public class CsvReadWrite
+{
+    private TMPro.TextMeshPro debugText;
+
+    private List<string[]> rowData = new List<string[]>();
+    private string filePath;
+    private string fileFormat = ".csv";
+
+    public CsvReadWrite(string fileName)
+    {
+        filePath = Path.Combine("C:/Data/Users/Mikae/Documents", fileName + fileFormat);
+
+        //debugText = GameObject.Find("debugText").GetComponent<TMPro.TextMeshPro>();
+        //debugText.text = filePath;
+
+        if (!File.Exists(filePath))
+        {
+            // Creating First row of titles manually..
+            string[] rowDataTemp = new string[4];
+            rowDataTemp[0] = "Angle";
+            rowDataTemp[1] = "X";
+            rowDataTemp[2] = "Y";
+            rowDataTemp[3] = "Z";
+            rowData.Add(rowDataTemp);
+
+            Save();
+        }
+    }
+
+    public void Save() 
+    {
+        string[][] output = new string[rowData.Count][];
+
+        for (int i = 0; i < output.Length; i++)
+        {
+            output[i] = rowData[i];
+        }
+
+        int length = output.GetLength(0);
+        string delimiter = ",";
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int index = 0; index < length; index++)
+            sb.AppendLine(string.Join(delimiter, output[index]));
+
+        using (StreamWriter outStream = new StreamWriter(filePath))
+        {
+            outStream.WriteLine(sb);
+            outStream.Close();
+        }
+    }
+
+    public void Write(float angle, float x, float y, float z)
+    {
+        string[] rowDataTemp = new string[4];
+        rowDataTemp[0] = Math.Round(angle,1).ToString();
+        rowDataTemp[1] = Math.Round(x, 2).ToString(); 
+        rowDataTemp[2] = Math.Round(y, 2).ToString(); 
+        rowDataTemp[3] = Math.Round(z, 2).ToString();
+        rowData.Add(rowDataTemp);
+  
+        Save();
+    }
+
+}
---
+++ Assets/Scripts/PunctureCoordinates.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PunctureCoordinates : MonoBehaviour
+{
+    public MeshCollider heartSurface;
+    public GameObject axis;
+    public TMPro.TextMeshPro punctureFeedbackText;
+    public AudioSource punctureFeedbackAudio;
+
+    bool hasCalculatedCoordinates = false;
+    float punctureAngle = 0;
+    Vector3 puncturePositon = Vector3.zero;
+    CsvReadWrite csvWriteCoordinates;
+
+    private void calculatePunctureCoordinates()
+    {
+        punctureAngle = Vector3.Angle(-axis.transform.up, gameObject.transform.right);
+        puncturePositon = gameObject.transform.position;
+
+        punctureFeedbackText.text = "Puncture coordinates calculated!";
+        punctureFeedbackAudio.Play();
+
+        csvWriteCoordinates.Write(punctureAngle, puncturePositon.x, puncturePositon.y, puncturePositon.z);
+
+        hasCalculatedCoordinates = true;
+    }
+
+    void OnTriggerStay(Collider collider)
+    {
+        if(collider.Equals(heartSurface) && !hasCalculatedCoordinates)
+        {
+            calculatePunctureCoordinates();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        csvWriteCoordinates = new CsvReadWrite("PunctureCoordinates");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
